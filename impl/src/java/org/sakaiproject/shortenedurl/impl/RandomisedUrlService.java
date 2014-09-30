6dc9b97849f3d1b9735207eca645d61dfebafb81
Nick Wilson <nick.wilson@it.ox.ac.uk>
2014-09-30 16:09:26 +0100

    WL-3090 URL shortener doesn't deal correctly with escaped characters in the URL being shortened.

--- impl/src/java/org/sakaiproject/shortenedurl/impl/RandomisedUrlService.java
+++ impl/src/java/org/sakaiproject/shortenedurl/impl/RandomisedUrlService.java
@@ -21,12 +21,9 @@
 
 package org.sakaiproject.shortenedurl.impl;
 
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.sql.SQLException;
 
+import org.apache.commons.httpclient.util.URIUtil;
 import org.apache.commons.lang.RandomStringUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
@@ -176,7 +173,8 @@ public class RandomisedUrlService extends HibernateDaoSupport implements Shorten
 		//first check cache
 		if(cache.containsKey(key)){
 			log.debug("Fetching url from cache for key: " + key);
-			return (String)cache.get(key);
+			String url = (String)cache.get(key);
+			return encodeUrl(url);
 		}
 		
 		//then check db
@@ -405,10 +403,8 @@ public class RandomisedUrlService extends HibernateDaoSupport implements Shorten
   		String encodedUrl = null;
   		
   		try {
-	  		URL url = new URL(rawUrl);
-	  		URI uri = new URI(url.getProtocol(), url.getUserInfo(), url.getHost(), url.getPort(), url.getPath(), url.getQuery(), url.getRef());
-	  		encodedUrl = uri.toURL().toString();
-  		} catch (Exception e) {
+			encodedUrl = URIUtil.encodeQuery(rawUrl);
+		} catch (Exception e) {
 	  		log.debug("Error encoding url: " + rawUrl +". " + e.getClass() + ": " + e.getMessage());
 		}
   		
